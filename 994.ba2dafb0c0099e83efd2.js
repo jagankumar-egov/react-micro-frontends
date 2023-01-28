067c08c3adec34cb97890b45f3f0c0e5874b81d2
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-01-28 18:50:10 +0530

    Updates

--- 994.ba2dafb0c0099e83efd2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcontainer=self.webpackChunkcontainer||[]).push([[994],{994:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>c});var r=t(212),s=t.n(r),u=t(896);const c=({login:e,history:n})=>{const t=(0,r.useRef)(null);return(0,r.useEffect)((()=>{(0,u.mount)(t.current,{login:e,history:n})}),[]),s().createElement("div",{ref:t})}}}]);
